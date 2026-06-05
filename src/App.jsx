e57bc160dfe3826283705e4c9ec49ac99ef3c973
davyson <davyssonlevi2.0@gmail.com>
2026-06-05 14:22:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export default function App() {
+  
+  return(
+    <div>
+      <h1> Agendamento de Remedios</h1>
+      <h2> Ajudamos na sua rotina de medicações </h2>
+    </div>
+  )
+}
