0d8bc1e13ffde9a12fea213782c64eee63594f3c
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2026-06-09 00:36:28 +0000

    terminanndo estilização do projeto pessoal de pw

--- src/components/VisorDeRemedios/index.jsx
+++ src/components/VisorDeRemedios/index.jsx
@@ -1,11 +1,12 @@
 import CardRemedio from "../CardRemedio";
+import "./style.css"
 
 export default function VisorDeRemedios({ remediosAgendados }) {
     console.log(remediosAgendados)
     return(
-        <div>
+        <div className="visorRemedios">
             {remediosAgendados.map((item, index) => (
-                <div key={index}>
+                <div className="remedio" key={index}>
                 <CardRemedio nome={item.nome} quantidade={item.quatidade} tempoRestante={item.tempoRestante} />
                 </div>
             ))}
